2a5855e34c928b0876c43dcc743aec40921bf504
JesseJohal89 <111985454+JesseJohal89@users.noreply.github.com>
2023-04-06 03:56:11 +0100

    Create math_magic.java
    In this project, you will become a mathemagician and write a small program that performs a mathematical magic trick! It will involve performing arithmetic operations on an integer that you choose.

---
+++ math_magic.java
@@ -0,0 +1,31 @@
+/*
+Java Exercise
+Math Magic
+
+In this project, you will become a mathemagician and write a small program that performs a mathematical magic trick! It will involve performing arithmetic operations on an integer that you choose.
+
+*/
+
+
+public class Magic {
+	public static void main(String[] args) {
+
+    int myNumber = 2;
+    // myNumber is the original number
+    
+    int stepOne = myNumber * myNumber;
+    
+    int stepTwo = stepOne + myNumber;
+
+    int stepThree = stepTwo + myNumber;
+    
+    int stepFour = stepThree + 17;
+
+    int stepFive = stepFour - myNumber;
+    
+    int stepSix = stepFive / 6;
+
+    System.out.println(stepSix);
+
+	}
+}
